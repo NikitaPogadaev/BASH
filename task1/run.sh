70d77f624ca5237f9c5feafc9d946319503d8944
Nikita <pognick@mail.ru>
2021-02-28 22:12:20 +0300

    dfgdf

--- task1/run.sh
+++ task1/run.sh
@@ -1,5 +1,5 @@
 # ./run.sh <путь к директории> <расширение> <название папки для бэкапа> <имя архива с бэкапа>
 mkdir $3
-find $1 -name "*.&2" -exec cp --backup=numbered {} $3 \;
+find $1 -name "*.$2" -exec cp --backup=numbered {} $3 \;
 tar -czf $4 $3
 echo done
